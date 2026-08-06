8b1065787cf95115fcb36e6316c46cc65fc7e8d2
MayarAbdelbaki <mayarelshamy50@gmail.com>
2026-08-06 13:14:05 +0300

    vat

---
+++ l10n_sa_vat_report_translation/__manifest__.py
@@ -0,0 +1,15 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Saudi VAT Report Arabic Translation',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting/Localizations',
+    'summary': "Arabic labels for the KSA VAT Report's row names, left untranslated upstream in l10n_sa",
+    'description': " ",  # Non-empty string to avoid loading the README file.
+    'depends': ['l10n_sa'],
+    'data': [],
+    'author': 'Nabie Alhuda',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
